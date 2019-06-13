f1a3e847f78991dd8b45256e45d832b1f08d857e
RogozhinVladislav <rogojinvladislav@gmail.com>
2019-06-13 13:05:40 +0300

    change server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use(express.static(__dirname + "/dist"));
 app.get(/.*/, function(req, res) {
     res.sendFile(__dirname + "/dist/index.html")
 });
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
