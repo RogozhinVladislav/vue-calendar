6a1ab80ec7766e7cfe13a1ee17ca76e84cb8cd44
RogozhinVladislav <rogojinvladislav@gmail.com>
2019-06-13 14:36:28 +0300

    deploy

---
+++ dist/precache-manifest.29adbec6d811b3a33591dfbd75305c85.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "0bcbe63d8af58381bd29",
+    "url": "/css/chunk-vendors.feec95b9.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "5350b434ff2ed570a394",
+    "url": "/js/app.3487fdf7.js"
+  },
+  {
+    "revision": "0bcbe63d8af58381bd29",
+    "url": "/js/chunk-vendors.f8c071d8.js"
+  },
+  {
+    "revision": "d2d6a062ddf5013d988cf027bbc7d69b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de07b398937d72eabc64e59cf07c53dd",
+    "url": "/font.woff2"
+  },
+  {
+    "revision": "7b39993203062be826b758c2c6493296",
+    "url": "/img/avatar1.png"
+  },
+  {
+    "revision": "f30f1bb876dd11e70b1dd66d42068afd",
+    "url": "/img/avatar.png"
+  },
+  {
+    "revision": "5350b434ff2ed570a394",
+    "url": "/css/app.90bb209e.css"
+  }
+];
