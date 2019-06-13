e32e13029166e565f752b6de54d0854033034a78
RogozhinVladislav <rogojinvladislav@gmail.com>
2019-06-13 11:59:33 +0300

    build для heroku

---
+++ dist/precache-manifest.6d5d3c9301f34cca01c0493e0b5e55f1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "5cf99c51ca5cfcfb8bae",
+    "url": "/css/chunk-vendors.61b39f47.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "a089f63fe93b46949ddc",
+    "url": "/js/app.75e1e1a2.js"
+  },
+  {
+    "revision": "5cf99c51ca5cfcfb8bae",
+    "url": "/js/chunk-vendors.ec4c3d71.js"
+  },
+  {
+    "revision": "9b648f1bfd32e22c4e12f17ae0ee60b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b39993203062be826b758c2c6493296",
+    "url": "/img/avatar1.png"
+  },
+  {
+    "revision": "f30f1bb876dd11e70b1dd66d42068afd",
+    "url": "/img/avatar.png"
+  },
+  {
+    "revision": "de07b398937d72eabc64e59cf07c53dd",
+    "url": "/font.woff2"
+  },
+  {
+    "revision": "a089f63fe93b46949ddc",
+    "url": "/css/app.f5266b93.css"
+  }
+];
